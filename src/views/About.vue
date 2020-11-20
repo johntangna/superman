6903570a341d173dd3c7b6f940ab7fb2279222e9
williem <1092473788@qq.com>
2020-11-20 13:36:42 +0800

    文件内容追加

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,25 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <div v-for="a in arr" :style="computedStyle">{{a}}</div>
   </div>
 </template>
+<script>
+  export default{
+    name : 'about',
+    data(){
+      return {
+        arr : ['表体1','标题2','标题3','标题4']
+      }
+    },
+    computed:{
+      computedStyle(){
+        for(let [k,v] of this.arr.entries()){
+          
+        }
+        return {
+          width :  160  + 'px'
+        }
+      }
+    }
+  }
+</script>
